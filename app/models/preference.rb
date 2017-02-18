e31a663c9caa1fd7b03353ffd1bb54dedabaa9a8
Angel <a.angel.martinez@gmail.com>
2017-02-17 20:20:30 -0500

    Change typeform id for Signup page and for API pull for preferences

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -4,21 +4,20 @@ class Preference < ActiveRecord::Base
 
 
   def self.filter_preferences(params)
-    answers = HTTParty.get("https://api.typeform.com/v0/form/Ugamap?key=#{ENV['typeform_key']}&token=#{params['form_response']['token']}").parsed_response['responses'][0]['answers']
+    answers = HTTParty.get("https://api.typeform.com/v0/form/BFdO9W?key=#{ENV['typeform_key']}&token=#{params['form_response']['token']}").parsed_response['responses'][0]['answers']
     create_preferences_for_user(answers)
   end
 
   def self.create_preferences_for_user(answers)
     Preference.create(
-      style_on_course:        answers['listimage_21263859_choice'],
-      dress_for_work:         answers['listimage_21263860_choice'],
-      shirt_fit:              answers['listimage_21263861_choice'],
-      pant_fit:               answers['listimage_21263862_choice'],
-      shirt_size:             answers['list_21263863_choice'],
-      waist_size:             answers['list_21263864_choice'],
-      pant_length:            answers['list_21263865_choice'],
-      philosophy_on_clothing: answers['list_21263866_choice'],
-      email:                  answers['email_21263871'])
+      style_on_course:        answers['listimage_40419493_choice'],
+      dress_for_work:         answers['listimage_40419494_choice'],
+      shirt_fit:              answers['listimage_40419491_choice'],
+      pant_fit:               answers['listimage_40419492_choice'],
+      shirt_size:             answers['list_40419481_choice'],
+      waist_size:             answers['list_40419482_choice'],
+      pant_length:            answers['list_40419483_choice'],
+      email:                  answers['email_40419504'])
   end
 
 end
